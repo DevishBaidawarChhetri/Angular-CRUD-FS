4066607649dfa46e83fac028c3f52aea74f539e6
Devish Baidawar Chhetri <devishbaidawar@gmail.com>
2021-05-26 12:02:03 +0545

    Login created

--- server/routers/user.js
+++ server/routers/user.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
-const User = require("../models/userSchema");
+const jwt = require("jsonwebtoken");
+const UserProvider = require("../models/userSchema");
 
 const router = express.Router();
 
 /**
- * @route POST /api/user
+ * @route POST /api/user/signup
  * @desc User Signup
  * @access Public
  */
@@ -18,14 +19,14 @@ router.post("/signup", async (req, res) => {
         message: "Don't leave fields empty!",
       });
     }
-    const userExist = await User.findOne({ email: req.body.email });
+    const userExist = await UserProvider.findOne({ email: req.body.email });
     if (userExist) {
       return res.status(422).json({
         message: "Email already exist!",
       });
     }
     const hashPassword = await bcrypt.hash(password, 10);
-    const user = new User({ fullName, email, password: hashPassword });
+    const user = new UserProvider({ fullName, email, password: hashPassword });
     await user.save();
     res.status(201).json({
       message: "Signup Successful!",
@@ -34,6 +35,45 @@ router.post("/signup", async (req, res) => {
     res.status(500).json({
       message: "Server Error!",
     });
+  }
+});
+
+/**
+ * @route POST /api/user/login
+ * @desc User Login
+ * @access Public
+ */
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserProvider.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid Credentials!",
+      });
+    }
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+      return res.status(401).json({
+        message: "Invalid Credentials!",
+      });
+    }
+    const token = jwt.sign(
+      {
+        email: user.email,
+        userId: user._id,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({
+      message: `Welcome, ${user.fullName}`,
+      token,
+      expiresIn: 3600,
+      userId: user._id,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Server error!" });
   }
 });
 
